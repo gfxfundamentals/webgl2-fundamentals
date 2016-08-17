65d4b934387a82d7c9e9ee6820a8b418577da42a
Gregg Tavares <github@greggman.com>
2016-08-17 19:15:42 +0900

    use input range for sliders
    no more jquery

--- webgl/resources/webgl-lessons-helper.js
+++ webgl/resources/webgl-lessons-helper.js
@@ -203,7 +203,51 @@
 
   updateCSSIfInIFrame();
 
-  window.setupLesson = setupLesson;
-  window.showNeedWebGL2 = showNeedWebGL2;
+  function setupSlider(selector, options) {
+    var precision = options.precision || 0;
+    var min = options.min || 0;
+    var step = options.step || 1;
+    var value = options.value || 0;
+    var max = options.max || 1;
+    var fn = options.slide;
+
+    min /= step;
+    max /= step;
+    value /= step;
+
+    var parent = document.querySelector(selector);
+    parent.innerHTML = `
+      <div class="gman-slider-outer">
+        <div class="gman-slider-upper">
+          <div class="gman-slider-label">${selector.substring(1)}</div>
+          <div class="gman-slider-value"></div>
+        </div>
+        <input class="gman-slider-slider" type="range" min="${min}" max="${max}" value="${value}" />
+      </div>
+    `;
+    var valueElem = parent.querySelector(".gman-slider-value");
+    var sliderElem = parent.querySelector(".gman-slider-slider");
+
+    function updateValue(value) {
+      valueElem.textContent = (value * step).toFixed(precision);
+    }
+
+    updateValue(value);
+
+    function handleChange(event) {
+      var value = parseInt(event.target.value);
+      updateValue(value);
+      fn(event, { value: value * step });
+    }
+
+    sliderElem.addEventListener('input', handleChange);
+    sliderElem.addEventListener('change', handleChange);
+  }
+
+  window.WebGLLessonsHelper = {
+    setupLesson: setupLesson,
+    showNeedWebGL2: showNeedWebGL2,
+    setupSlider: setupSlider,
+  };
 }(this));
 
