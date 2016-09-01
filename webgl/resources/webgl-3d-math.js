826f7376089df280535c5b5b5ce2a74e6a4fe646
Gregg Tavares <github@greggman.com>
2016-09-01 13:06:17 +0900

    use optimized math

--- webgl/resources/webgl-3d-math.js
+++ webgl/resources/webgl-3d-math.js
@@ -449,7 +449,48 @@
    * @memberOf module:webgl-3d-math
    */
   function translate(m, tx, ty, tz, dst) {
-    return multiply(m, translation(tx, ty, tz), dst);
+    // This is the optimized version of
+    // return multiply(m, translation(tx, ty, tz), dst);
+    dst = dst || new Float32Array(16);
+
+    var m00 = m[0];
+    var m01 = m[1];
+    var m02 = m[2];
+    var m03 = m[3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var m30 = m[3 * 4 + 0];
+    var m31 = m[3 * 4 + 1];
+    var m32 = m[3 * 4 + 2];
+    var m33 = m[3 * 4 + 3];
+
+    if (m !== dst) {
+      dst[ 0] = m00;
+      dst[ 1] = m01;
+      dst[ 2] = m02;
+      dst[ 3] = m03;
+      dst[ 4] = m10;
+      dst[ 5] = m11;
+      dst[ 6] = m12;
+      dst[ 7] = m13;
+      dst[ 8] = m20;
+      dst[ 9] = m21;
+      dst[10] = m22;
+      dst[11] = m23;
+    }
+
+    dst[12] = m00 * tx + m10 * ty + m20 * tz + m30;
+    dst[13] = m01 * tx + m11 * ty + m21 * tz + m31;
+    dst[14] = m02 * tx + m12 * ty + m22 * tz + m32;
+    dst[15] = m03 * tx + m13 * ty + m23 * tz + m33;
+
+    return dst;
   }
 
   /**
@@ -493,7 +534,42 @@
    * @memberOf module:webgl-3d-math
    */
   function xRotate(m, angleInRadians, dst) {
-    return multiply(m, xRotation(angleInRadians), dst);
+    // this is the optimized version of
+    // return multiply(m, xRotation(angleInRadians), dst);
+    dst = dst || new Float32Array(16);
+
+    var m10 = m[4];
+    var m11 = m[5];
+    var m12 = m[6];
+    var m13 = m[7];
+    var m20 = m[8];
+    var m21 = m[9];
+    var m22 = m[10];
+    var m23 = m[11];
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[4]  = c * m10 + s * m20;
+    dst[5]  = c * m11 + s * m21;
+    dst[6]  = c * m12 + s * m22;
+    dst[7]  = c * m13 + s * m23;
+    dst[8]  = c * m20 - s * m10;
+    dst[9]  = c * m21 - s * m11;
+    dst[10] = c * m22 - s * m12;
+    dst[11] = c * m23 - s * m13;
+
+    if (m !== dst) {
+      dst[ 0] = m[ 0];
+      dst[ 1] = m[ 1];
+      dst[ 2] = m[ 2];
+      dst[ 3] = m[ 3];
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
   }
 
   /**
@@ -537,7 +613,42 @@
    * @memberOf module:webgl-3d-math
    */
   function yRotate(m, angleInRadians, dst) {
-    return multiply(m, yRotation(angleInRadians), dst);
+    // this is the optimized verison of
+    // return multiply(m, yRotation(angleInRadians), dst);
+    dst = dst || new Float32Array(16);
+
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[ 0] = c * m00 - s * m20;
+    dst[ 1] = c * m01 - s * m21;
+    dst[ 2] = c * m02 - s * m22;
+    dst[ 3] = c * m03 - s * m23;
+    dst[ 8] = c * m20 + s * m00;
+    dst[ 9] = c * m21 + s * m01;
+    dst[10] = c * m22 + s * m02;
+    dst[11] = c * m23 + s * m03;
+
+    if (m !== dst) {
+      dst[ 4] = m[ 4];
+      dst[ 5] = m[ 5];
+      dst[ 6] = m[ 6];
+      dst[ 7] = m[ 7];
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
   }
 
   /**
@@ -581,7 +692,42 @@
    * @memberOf module:webgl-3d-math
    */
   function zRotate(m, angleInRadians, dst) {
-    return multiply(m, zRotation(angleInRadians), dst);
+    // This is the optimized verison of
+    // return multiply(m, zRotation(angleInRadians), dst);
+    dst = dst || new Float32Array(16);
+
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[ 0] = c * m00 + s * m10;
+    dst[ 1] = c * m01 + s * m11;
+    dst[ 2] = c * m02 + s * m12;
+    dst[ 3] = c * m03 + s * m13;
+    dst[ 4] = c * m10 - s * m00;
+    dst[ 5] = c * m11 - s * m01;
+    dst[ 6] = c * m12 - s * m02;
+    dst[ 7] = c * m13 - s * m03;
+
+    if (m !== dst) {
+      dst[ 8] = m[ 8];
+      dst[ 9] = m[ 9];
+      dst[10] = m[10];
+      dst[11] = m[11];
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
   }
 
   /**
@@ -639,7 +785,68 @@
    * @memberOf module:webgl-3d-math
    */
   function axisRotate(m, axis, angleInRadians, dst) {
-    return multiply(m, axisRotation(axis, angleInRadians), dst);
+    // This is the optimized verison of
+    // return multiply(m, axisRotation(axis, angleInRadians), dst);
+    dst = dst || new Float32Array(16);
+
+    var x = axis[0];
+    var y = axis[1];
+    var z = axis[2];
+    var n = Math.sqrt(x * x + y * y + z * z);
+    x /= n;
+    y /= n;
+    z /= n;
+    var xx = x * x;
+    var yy = y * y;
+    var zz = z * z;
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+    var oneMinusCosine = 1 - c;
+
+    var r00 = xx + (1 - xx) * c;
+    var r01 = x * y * oneMinusCosine + z * s;
+    var r02 = x * z * oneMinusCosine - y * s;
+    var r10 = x * y * oneMinusCosine - z * s;
+    var r11 = yy + (1 - yy) * c;
+    var r12 = y * z * oneMinusCosine + x * s;
+    var r20 = x * z * oneMinusCosine + y * s;
+    var r21 = y * z * oneMinusCosine - x * s;
+    var r22 = zz + (1 - zz) * c;
+
+    var m00 = m[0];
+    var m01 = m[1];
+    var m02 = m[2];
+    var m03 = m[3];
+    var m10 = m[4];
+    var m11 = m[5];
+    var m12 = m[6];
+    var m13 = m[7];
+    var m20 = m[8];
+    var m21 = m[9];
+    var m22 = m[10];
+    var m23 = m[11];
+
+    dst[ 0] = r00 * m00 + r01 * m10 + r02 * m20;
+    dst[ 1] = r00 * m01 + r01 * m11 + r02 * m21;
+    dst[ 2] = r00 * m02 + r01 * m12 + r02 * m22;
+    dst[ 3] = r00 * m03 + r01 * m13 + r02 * m23;
+    dst[ 4] = r10 * m00 + r11 * m10 + r12 * m20;
+    dst[ 5] = r10 * m01 + r11 * m11 + r12 * m21;
+    dst[ 6] = r10 * m02 + r11 * m12 + r12 * m22;
+    dst[ 7] = r10 * m03 + r11 * m13 + r12 * m23;
+    dst[ 8] = r20 * m00 + r21 * m10 + r22 * m20;
+    dst[ 9] = r20 * m01 + r21 * m11 + r22 * m21;
+    dst[10] = r20 * m02 + r21 * m12 + r22 * m22;
+    dst[11] = r20 * m03 + r21 * m13 + r22 * m23;
+
+    if (m !== dst) {
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
   }
 
   /**
@@ -685,7 +892,31 @@
    * @memberOf module:webgl-3d-math
    */
   function scale(m, sx, sy, sz, dst) {
-    return multiply(m, scaling(sx, sy, sz), dst);
+    // This is the optimized verison of
+    // return multiply(m, scaling(sx, sy, sz), dst);
+    dst = dst || new Float32Array(16);
+
+    dst[ 0] = sx * m[0 * 4 + 0];
+    dst[ 1] = sx * m[0 * 4 + 1];
+    dst[ 2] = sx * m[0 * 4 + 2];
+    dst[ 3] = sx * m[0 * 4 + 3];
+    dst[ 4] = sy * m[1 * 4 + 0];
+    dst[ 5] = sy * m[1 * 4 + 1];
+    dst[ 6] = sy * m[1 * 4 + 2];
+    dst[ 7] = sy * m[1 * 4 + 3];
+    dst[ 8] = sz * m[2 * 4 + 0];
+    dst[ 9] = sz * m[2 * 4 + 1];
+    dst[10] = sz * m[2 * 4 + 2];
+    dst[11] = sz * m[2 * 4 + 3];
+
+    if (m !== dst) {
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
   }
 
   /**
