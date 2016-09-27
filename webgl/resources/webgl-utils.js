5764054ca4da6ffbcab742fb6a677346f6bf556b
Gregg Tavares <github@greggman.com>
2016-09-28 00:30:17 +0900

    fix resize so it works with non-int device pixel ratios

--- webgl/resources/webgl-utils.js
+++ webgl/resources/webgl-utils.js
@@ -751,8 +751,8 @@
    */
   function resizeCanvasToDisplaySize(canvas, multiplier) {
     multiplier = multiplier || 1;
-    var width  = canvas.clientWidth  * multiplier;
-    var height = canvas.clientHeight * multiplier;
+    var width  = canvas.clientWidth  * multiplier | 0;
+    var height = canvas.clientHeight * multiplier | 0;
     if (canvas.width !== width ||  canvas.height !== height) {
       canvas.width  = width;
       canvas.height = height;
