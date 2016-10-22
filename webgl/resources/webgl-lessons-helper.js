f38033cbaa91905e653c3aa1a8d6b6d85ec4ac33
Gregg Tavares <github@greggman.com>
2016-10-22 18:08:24 +0900

    handle context lost

--- webgl/resources/webgl-lessons-helper.js
+++ webgl/resources/webgl-lessons-helper.js
@@ -170,6 +170,25 @@
   function setupLesson(canvas, opt_options) {
     var options = opt_options || {};
 
+    if (canvas) {
+      canvas.addEventListener('webglcontextlost', function(e) {
+          // the default is to do nothing. Preventing the default
+          // means allowing context to be restored
+          e.preventDefault();
+          var div = document.createElement("div");
+          div.className = "contextlost";
+          div.innerHTML = '<div>Context Lost: Click To Reload</div>';
+          div.addEventListener('click', function() {
+              window.location.reload();
+          });
+          document.body.appendChild(div);
+      });
+      canvas.addEventListener('webglcontextrestored', function(e) {
+          // just reload the page. Easiest.
+          window.location.reload();
+      });
+    }
+
     if (isInIFrame()) {
       updateCSSIfInIFrame();
     } else if (!options.noTitle && options.title !== false) {
