10c29ab7f1d6d00f000a9e75fab4bc86a84ff758
Gregg Tavares <github@greggman.com>
2016-11-23 15:25:42 +0900

    fix diff check

--- deploy.sh
+++ deploy.sh
@@ -39,7 +39,7 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --quiet` ]; then
+if git diff --quiet ; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
