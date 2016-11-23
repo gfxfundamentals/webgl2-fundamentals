dc6e271b125ffe575cc73c09b204a1fb7838f376
Gregg Tavares <github@greggman.com>
2016-11-23 13:40:47 +0900

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,6 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-echo "git clone $REPO out"
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
@@ -40,12 +39,10 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
+if [ -z `git diff --quiet` ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
-
-echo "git commit changes"
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
@@ -61,8 +58,6 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out 
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
-
-echo "git push $SSH_REPO $TARGET_BRANCH"
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
