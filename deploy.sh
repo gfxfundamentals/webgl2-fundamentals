2aece4b7e29e2d04dbebf7dbde28258def876f5b
Gregg Tavares <github@greggman.com>
2016-11-23 15:38:53 +0900

    use git add -A

--- deploy.sh
+++ deploy.sh
@@ -46,7 +46,7 @@ fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
+git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
