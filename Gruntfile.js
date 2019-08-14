8fa0471dc1ff05bdc729110a6a992734b15b157f
Gregg Tavares <github@greggman.com>
2019-08-14 20:36:19 +0900

    add pre-push script

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,6 +158,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', ['clean', 'copy', 'buildlessons']);
   grunt.registerTask('buildwatch', ['build', 'watch']);
+  grunt.registerTask('pre-push', ['eslint']);
 
   grunt.registerTask('default', ['eslint', 'build', 'jsdoc']);
 };
