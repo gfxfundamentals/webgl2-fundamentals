be1370d7c891be5f308f6f2b97a0be2e16b69300
Gregg Tavares <github@greggman.com>
2019-08-30 17:30:25 +0900

    use abs
    not sure why I changed this

--- webgl/resources/same-code-02.js
+++ webgl/resources/same-code-02.js
@@ -48,7 +48,7 @@ uniform float u_specularFactor;
 
 vec4 lit(float l ,float h, float m) {
   return vec4(1.0,
-              pow(max(0.0, l), 2.5),
+              pow(abs(l), 2.5),
               (l > 0.0) ? pow(max(0.0, h), m) : 0.0,
               1.0);
 }
